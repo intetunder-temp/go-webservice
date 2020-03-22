85b470364f60f97558be92ac3d59a16d18f40630
intetunder-temp <github-temp@darknet.fyi>
2020-03-22 17:13:23 +0000

    adding stuff for the webservice

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/intetunder-temp/go-webservice/models"
+	"github.com/intetunder-temp/go-webservice/controllers"
 )
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Danstar",
-		LastName:  "Scarpa",
-	}
-	fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe("127.0.0.1:3000", nil)
 }
