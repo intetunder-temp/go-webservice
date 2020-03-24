c817d9de0cb67c61df2ea09e4ef814cc267e7342
intetunder-temp <github-temp@darknet.fyi>
2020-03-24 16:27:11 +0000

    finished the web service

--- models/user.go
+++ models/user.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
+// User : Users
 type User struct {
 	ID        int
 	FirstName string
@@ -11,13 +17,52 @@ var (
 	nextID = 1
 )
 
+// GetUsers : get users
 func GetUsers() []*User {
 	return users
 }
 
+// AddUser : Adding users
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("New User most not include id or it most be set")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+// GetUserByID : get user by ID
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+// UpdateUser : Updating user
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+// RemoverUserByID : Removing user by ID
+func RemoverUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
